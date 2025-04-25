3c5b18a80cf095f399e6f89bf930bf341bac29a6
abir246 <231004012@eastdelta.edu.bd>
2025-04-25 09:34:42 +0600

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,22 @@
+// src/assets/assets.js
+
+// Import all PNG image assets
+import logo from './logo.png';
+import search from './search.png';
+import addtocart from './addtocart.png';
+import categories from './categories.png';
+import exteriorparts from './exteriorparts.png';
+import interiorparts from './interiorparts.png';
+
+// Export them in a single object for easy access
+ const assets = {
+  logo,
+  search,
+  addtocart,
+  categories,
+  exteriorparts,
+  interiorparts,
+
+};
+
+export default assets;
